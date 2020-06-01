fa2a9251d850b1487a1d194b3419f82c4da7c9f0
ritikashamdasani <rshamdasani1@gmail.com>
2020-05-31 23:37:49 -0400

    Iteration 2

---
+++ ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Ritika Shamdasani on 5/31/20.
+//  Copyright © 2020 Ritika Shamdasani. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
